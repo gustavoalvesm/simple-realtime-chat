250216573e9f6e641f2bfe8b168797ff9f96cb46
Gustavo Alves <gustavoamguty@gmail.com>
2020-05-31 21:09:03 -0400

    Finish the structure and apply new basic design

--- node-server/app.js
+++ node-server/app.js
@@ -7,12 +7,31 @@ var server = app.listen(3000);
 var socket = require('socket.io');
 var io = socket(server);
 
+// Small history of chat
+var history = [];
+
+// Add new message in history
+function addInHistory(data){
+    if(history.length > 100) history.shift();
+    history.push(data);
+}
+
+// On new connection
 io.on('connection', (socket) => {
     
     // New message sended
     socket.on('chat-message', function (data) {
-        io.emit('chat-message', data);
-        console.log('new message from ' + data.username);
+        var msg = {
+            ...data,
+            created_at: new Date()
+        }
+        io.emit('chat-message', msg);
+        addInHistory(msg);
+    });
+
+    // Get history
+    socket.on('chat-history', function (){
+        socket.emit('chat-history', history);
     });
     
 });
--- simple-chat/src/App.js
+++ simple-chat/src/App.js
@@ -4,18 +4,33 @@ import io from 'socket.io-client';
 
 const socket = io('http://localhost:3000');
 
+function generateUsername() {
+  let date = new Date();
+  return `user-${date.getHours()}${date.getMinutes()}${date.getSeconds()}`;
+}
+
 function App() {
 
-  const [username, setUsername] = useState("Visitor");
+  const [username] = useState(generateUsername());
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
 
+  // Initialize listeners
   useEffect(() => {
     socket.on('chat-message', (data) => {
       setMessages([...messages, data]);
     });
+    socket.on('chat-history', (data) => {
+      setMessages(data);
+    });
   }, [messages]);
 
+  // Request a history
+  useEffect(() => {
+    socket.emit('chat-history');
+  }, []);
+
+  // Send a new message
   const sendMessage = (e) => {
     e.preventDefault();
     let data = { username, message };
@@ -23,17 +38,21 @@ function App() {
     setMessage("");
   }
 
+  // Process and render a message
+  const renderMessage = (msg, index) => {
+    const senderClass = msg.username === username ? 'App__MessagesBox__Message--sender' : '';
+    return (
+      <div className={`App__MessagesBox__Message ${senderClass}`} key={`msg${index}`}>
+        <div className="App__MessagesBox__Message__Username">{msg.username}</div>
+        <div className="App__MessagesBox__Message__Content">{msg.message}</div>
+      </div>
+    );
+  }
+
   return (
     <main className="App">
       <section className="App__MessagesBox">
-        {messages.map((msg, index) => {
-          return (
-            <div className="App__MessagesBox__Message" key={`msg${index}`}>
-              <div className="App__MessagesBox__Message__Username">{msg.username}</div>
-              <div className="App__MessagesBox__Message__Text">{msg.message}</div>
-            </div>
-          );
-        })}
+        {messages.map((msg, index) => renderMessage(msg, index))}
       </section>
       <form
         onSubmit={sendMessage}
@@ -44,6 +63,7 @@ function App() {
           onChange={(e) => setMessage(e.target.value)}
           value={message}
           required={true}
+          autoFocus
         />
       </form>
     </main>
